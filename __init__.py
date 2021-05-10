a94c206502831e49c8866b486927772e2fa6c995
Ömer Faruk Öz <ozomerfaruk98@gmail.com>
2021-05-10 13:57:57 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
+import bpy
 bl_info = {
     "name": "Short_Panel",
     "author": "Ömer Faruk Öz",
@@ -10,78 +11,67 @@ bl_info = {
     "category": "Interface",
 }
 
-import bpy
 
-i=[]
+i = []
+
+
 def selectf(name):
-    transformsobject=bpy.context.scene.objects
-    
+    transformsobject = bpy.context.scene.objects
+
     for index in range(len(transformsobject)):
         if transformsobject[index].select_get():
             i.append(transformsobject[index].name)
-    
-    
-    
+
     ob = bpy.context.scene.objects[name.name]
     bpy.ops.object.select_all(action='DESELECT')
     bpy.context.view_layer.objects.active = ob
-    ob.select_set(True) 
+    ob.select_set(True)
 
 
 def oldSelectf():
-    
-    transformsobject=bpy.context.scene.objects
+
+    transformsobject = bpy.context.scene.objects
     bpy.ops.object.select_all(action='DESELECT')
     for select in range(len(transformsobject)):
         for selected in range(len(i)):
-            if i[selected]==transformsobject[select].name:
-                z=bpy.context.scene.objects[transformsobject[select].name]
+            if i[selected] == transformsobject[select].name:
+                z = bpy.context.scene.objects[transformsobject[select].name]
                 z.select_set(True)
     i.clear()
 
+
 def set(name):
-    
+
     selectf(name)
-    
+
     """area"""
-    
-    bpy.ops.object.duplicate ()
-    newPlace=bpy.context.active_object
+
+    bpy.ops.object.duplicate()
+    newPlace = bpy.context.active_object
     bpy.ops.object.transform_apply(location=False, rotation=True, scale=True)
-    
+
     """area"""
-    
+
     oldSelectf()
-                
-    z=bpy.context.scene.objects[name.name]
+
+    z = bpy.context.scene.objects[name.name]
     z.select_set(False)
-    
+
     newPlace.select_set(True)
-    
-    
+
     return newPlace
-    
 
 
-
-
-
-
-
-#Unity Transform operator
+# Unity Transform operator
 
 def buttonunity(context):
-    if  bpy.context.active_object.mode == 'OBJECT':
-        bpy.ops.object.transform_apply(location=False, rotation=True, scale=False)
+    if bpy.context.active_object.mode == 'OBJECT':
+        bpy.ops.object.transform_apply(
+            location=False, rotation=True, scale=False)
         bpy.context.object.rotation_euler[0] = -1.5708
-        bpy.ops.object.transform_apply(location=False, rotation=True, scale=False)
+        bpy.ops.object.transform_apply(
+            location=False, rotation=True, scale=False)
         bpy.context.object.rotation_euler[0] = 1.5708
-    
-
-
-
-
-
 
 
 class UnityTransform(bpy.types.Operator):
@@ -98,59 +88,52 @@ class UnityTransform(bpy.types.Operator):
         return {'FINISHED'}
 
 
-#Welding Normal operator
+# Welding Normal operator
 
 def welding(context):
-    if  bpy.context.active_object.mode == 'OBJECT' and len(bpy.context.selected_objects)==2:
-        
-        area=bpy.context.area.ui_type
+    if bpy.context.active_object.mode == 'OBJECT' and len(bpy.context.selected_objects) == 2:
+
+        area = bpy.context.area.ui_type
         bpy.context.area.ui_type = 'VIEW_3D'
 
-        active=bpy.context.active_object
-        selected=bpy.context.active_object
+        active = bpy.context.active_object
+        selected = bpy.context.active_object
 
         for object in bpy.context.selected_objects:
-            if not object==active:
-                selected=object
-
+            if not object == active:
+                selected = object
 
         active.data.use_auto_smooth = True
         bpy.ops.object.shade_smooth()
 
-
-        #vertex group
+        # vertex group
         bpy.ops.object.editmode_toggle()
-        allGroup=active.vertex_groups.new()
+        allGroup = active.vertex_groups.new()
         bpy.ops.mesh.select_all(action='SELECT')
         bpy.ops.object.vertex_group_assign()
         bpy.ops.object.editmode_toggle()
 
-
-        #vertexWeightProximity modifier
-        vertexWeight=active.modifiers.new('vertexWeightProximity(sp)', 'VERTEX_WEIGHT_PROXIMITY')
+        # vertexWeightProximity modifier
+        vertexWeight = active.modifiers.new(
+            'vertexWeightProximity(sp)', 'VERTEX_WEIGHT_PROXIMITY')
         vertexWeight.vertex_group = allGroup.name
-        vertexWeight.target=selected
+        vertexWeight.target = selected
         vertexWeight.proximity_mode = 'GEOMETRY'
         vertexWeight.proximity_geometry = {'FACE'}
         vertexWeight.max_dist = 0.2
         vertexWeight.falloff_type = 'SMOOTH'
 
-        #DataTransfer modifier
-        dataTransfer=active.modifiers.new('DataTransfer(sp)', 'DATA_TRANSFER')
-        dataTransfer.object=selected
-        dataTransfer.vertex_group=allGroup.name
+        # DataTransfer modifier
+        dataTransfer = active.modifiers.new(
+            'DataTransfer(sp)', 'DATA_TRANSFER')
+        dataTransfer.object = selected
+        dataTransfer.vertex_group = allGroup.name
         dataTransfer.invert_vertex_group = True
         dataTransfer.use_loop_data = True
         dataTransfer.data_types_loops = {'CUSTOM_NORMAL'}
         dataTransfer.loop_mapping = 'POLYINTERP_NEAREST'
 
         bpy.context.area.ui_type = area
-        
-
-
-
-
-
 
 
 class WeldingNormal(bpy.types.Operator):
@@ -167,49 +150,80 @@ class WeldingNormal(bpy.types.Operator):
         return {'FINISHED'}
 
 
+# Bend Normal operator
 
-#droop operator
+def bend(context):
+    if bpy.context.active_object.mode == 'OBJECT' and len(bpy.context.selected_objects) == 2:
+        import bpy
+
+        area = bpy.context.area.ui_type
+        bpy.context.area.ui_type = 'VIEW_3D'
+
+        for object in bpy.context.selected_objects:
+            # texture
+            texture = bpy.data.textures.new('displace(sp)', 'CLOUDS')
+            texture.noise_scale = 2
+            texture.cloud_type = 'COLOR'
+
+            # displace modifier
+            displace = object.modifiers.new('displace(sp)', 'DISPLACE')
+            displace.texture = texture
+            displace.texture_coords = 'GLOBAL'
+            displace.direction = 'RGB_TO_XYZ'
+            displace.strength = 0.1
+
+        bpy.context.area.ui_type = area
+
+
+class Bend(bpy.types.Operator):
+    """Tooltip"""
+    bl_idname = "object.bend"
+    bl_label = "Bend"
+
+    @classmethod
+    def poll(cls, context):
+        return context.active_object is not None
+
+    def execute(self, context):
+        bend(context)
+        return {'FINISHED'}
+
+
+# droop operator
 
 def buttondroop(context):
-    if  bpy.context.active_object.mode == 'OBJECT' and bpy.context.view_layer.objects.active != None:
+    if bpy.context.active_object.mode == 'OBJECT' and bpy.context.view_layer.objects.active != None:
         if bpy.context.view_layer.objects.active.select_get():
-            
+
             for object in bpy.context.selected_objects:
                 if object == bpy.context.active_object:
-                    oldplace=object.name
-                    place=set(object)
+                    oldplace = object.name
+                    place = set(object)
                     for object in bpy.context.selected_objects:
                         if object != bpy.context.active_object:
-                            oldRotationZ=object.rotation_euler.z
-                            object.rotation_euler = (0,0,0)
-                            constraint= object.constraints.new('SHRINKWRAP')
-                            
+                            oldRotationZ = object.rotation_euler.z
+                            object.rotation_euler = (0, 0, 0)
+                            constraint = object.constraints.new('SHRINKWRAP')
+
                             constraint.shrinkwrap_type = 'PROJECT'
                             constraint.project_axis = 'NEG_Z'
-                            constraint.target=place
+                            constraint.target = place
                             constraint.use_track_normal = True
-                            constraint.track_axis = 'TRACK_Z' 
+                            constraint.track_axis = 'TRACK_Z'
                             selectf(object)
                             bpy.ops.object.visual_transform_apply()
                             oldSelectf()
-                            
+
                             object.constraints.remove(constraint)
-                            object.rotation_euler.rotate_axis("Z", oldRotationZ)
-                            
-                            
-                            
-                            
+                            object.rotation_euler.rotate_axis(
+                                "Z", oldRotationZ)
+
             ob = bpy.context.scene.objects[oldplace]
             bpy.context.view_layer.objects.active = ob
-            ob.select_set(True) 
-            
+            ob.select_set(True)
+
             objs = [bpy.context.scene.objects[place.name]]
             bpy.ops.object.delete({"selected_objects": objs})
-                    
-                    
-                    
-                    
-                    
 
 
 class Droop(bpy.types.Operator):
@@ -226,39 +240,32 @@ class Droop(bpy.types.Operator):
         return {'FINISHED'}
 
 
-
-#Denoise operator 
+# Denoise operator
 
 
 def buttondenois(context):
-    
+
     bpy.context.scene.use_nodes = True
     bpy.context.scene.render.engine = 'CYCLES'
     bpy.context.view_layer.cycles.denoising_store_passes = True
-    
-    
+
     scene = bpy.context.scene
-    
+
     nodetree = scene.node_tree
-    
+
     node1 = nodetree.nodes.new("CompositorNodeDenoise")
 
-    
     node2 = nodetree.nodes.new("CompositorNodeRLayers")
-    node2.location = (-300,0)
-    
-    
+    node2.location = (-300, 0)
+
     for a in nodetree.nodes:
         if a.type == 'COMPOSITE':
-            node0=a
-    
-    
-    nodetree.links.new(node2.outputs[3],node1.inputs[0])
-    nodetree.links.new(node2.outputs[4],node1.inputs[1])
-    nodetree.links.new(node2.outputs[5],node1.inputs[2])
-    nodetree.links.new(node1.outputs[0],node0.inputs[0])
-    
+            node0 = a
 
+    nodetree.links.new(node2.outputs[3], node1.inputs[0])
+    nodetree.links.new(node2.outputs[4], node1.inputs[1])
+    nodetree.links.new(node2.outputs[5], node1.inputs[2])
+    nodetree.links.new(node1.outputs[0], node0.inputs[0])
 
 
 class Denoiseshot(bpy.types.Operator):
@@ -274,45 +281,42 @@ class Denoiseshot(bpy.types.Operator):
         buttondenois(context)
         return {'FINISHED'}
 
-#Set Orgin operator 
+# Set Orgin operator
+
 
 def buttonorgin(context):
     if bpy.context.active_object.mode == 'EDIT':
-        cursor=(bpy.context.scene.cursor.location.x,bpy.context.scene.cursor.location.y,bpy.context.scene.cursor.location.z)
-        
-        area=bpy.context.area.type
+        cursor = (bpy.context.scene.cursor.location.x,
+                  bpy.context.scene.cursor.location.y, bpy.context.scene.cursor.location.z)
+
+        area = bpy.context.area.type
         bpy.context.area.type = 'VIEW_3D'
         bpy.ops.view3d.snap_cursor_to_selected()
         bpy.context.area.type = area
         bpy.ops.object.editmode_toggle()
-        
+
         selectf(bpy.context.active_object)
-        visible=[]
+        visible = []
         modifierList = bpy.context.object.modifiers.keys()
-        
+
         for modifier in modifierList:
-            if bpy.context.object.modifiers[modifier].show_viewport ==True:
+            if bpy.context.object.modifiers[modifier].show_viewport == True:
                 visible.append(True)
                 bpy.context.object.modifiers[modifier].show_viewport = False
             else:
                 visible.append(False)
-           
-           
+
         bpy.ops.object.origin_set(type='ORIGIN_CURSOR', center='MEDIAN')
-        
-        
+
         for index in range(len(modifierList)):
             if visible[index]:
-                bpy.context.object.modifiers[modifierList[index]].show_viewport = True
+                bpy.context.object.modifiers[modifierList[index]
+                                             ].show_viewport = True
         oldSelectf()
-        
+
         bpy.ops.object.editmode_toggle()
-        
-        bpy.context.scene.cursor.location=cursor
-        
 
-
-
+        bpy.context.scene.cursor.location = cursor
 
 
 class Orgin(bpy.types.Operator):
@@ -327,9 +331,6 @@ class Orgin(bpy.types.Operator):
     def execute(self, context):
         buttonorgin(context)
         return {'FINISHED'}
-
-
-
 
 
 class ShortPanel(bpy.types.Panel):
@@ -358,21 +359,21 @@ class ShortPanel(bpy.types.Panel):
 
         row = layout.row()
         row.operator("object.welding_normal")
-        
+
         row = layout.row()
         row.operator("object.droop")
-        
+
         row = layout.row()
         row.operator("object.denoiseshot")
-        
+
         row = layout.row()
         row.operator("object.orgin")
-        
-        
-        
+
+
 def register():
     bpy.utils.register_class(UnityTransform)
     bpy.utils.register_class(WeldingNormal)
+    bpy.utils.register_class(Bend)
     bpy.utils.register_class(Droop)
     bpy.utils.register_class(Denoiseshot)
     bpy.utils.register_class(Orgin)
@@ -382,6 +383,7 @@ def register():
 def unregister():
     bpy.utils.unregister_class(UnityTransform)
     bpy.utils.unregister_class(WeldingNormal)
+    bpy.utils.unregister_class(Bend)
     bpy.utils.unregister_class(Droop)
     bpy.utils.unregister_class(Denoiseshot)
     bpy.utils.unregister_class(Orgin)
